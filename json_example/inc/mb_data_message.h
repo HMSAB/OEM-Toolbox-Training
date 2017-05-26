3226c38aa542d7418d5d4059480f133a058e7dab
Ian Tracy <iatr@hms.se>
2017-05-26 14:29:12 -0400

    request function changed

--- json_example/inc/mb_data_message.h
+++ json_example/inc/mb_data_message.h
@@ -5,16 +5,16 @@
 #include <string>
 #include "message.h"
 
-class mb_data_message {
+class mb_data_message
+{
 private:
   uuid_t guid;
   char guid_string[GUID_SIZE];
   int reg, slaveid, port;
-  std::string function;
-  std::string type;
   std::string ip;
 
-  void gen_uuid(void) {
+  void gen_uuid(void)
+  {
     uuid_generate(this->guid);
     uuid_unparse(guid, guid_string);
     // std::cout << guid_string << std::endl;
@@ -22,10 +22,9 @@ private:
 
 public:
   void set_register(int reg) { this->reg = reg; }
-  void request(int reg, const char *command, const char *type,
-               const char *function);
-  /*constructor*/ 
-    mb_data_message(int slaveid, int port, std::string ip);
+  void request(int reg, std::string command, std::string type, std::string function);
+  /*constructor*/
+  mb_data_message(int slaveid, int port, std::string ip);
 };
 
 #endif
--- json_example/src/mb_data_message.cpp
+++ json_example/src/mb_data_message.cpp
@@ -6,11 +6,9 @@
 #include <string>
 #include <uuid/uuid.h>
 
-void mb_data_message::request(int reg, const char *command, const char *type, const char *function)
+
+void mb_data_message::request(int reg, std::string command, std::string type, std::string function)
 {
-    //this->reg = reg;
-    //this->type = type;
-    //this->function = function;
     char *s = NULL;
 
     json_t *root = json_object();
@@ -21,14 +19,14 @@ void mb_data_message::request(int reg, const char *command, const char *type, co
     json_t *address_json = json_object();
     json_t *value_json = json_object();
 
-    json_object_set_new(command_json, "command", json_string(command));
-    json_object_set_new(address_json, "function", json_string(function));
+    json_object_set_new(command_json, "command", json_string(command.c_str()));
+    json_object_set_new(address_json, "function", json_string(function.c_str()));
     json_object_set_new(address_json, "slave_id", json_integer(slaveid));
     json_object_set_new(address_json, "register", json_integer(reg));
     json_object_set_new(address_json, "ip_address", json_string(ip.c_str()));
     json_object_set_new(address_json, "ip_port", json_integer(port));
 
-    json_object_set_new(value_json, "value_type", json_string(type));
+    json_object_set_new(value_json, "value_type", json_string(type.c_str()));
     json_object_set_new(command_json, "address", address_json);
     json_object_set_new(command_json, "value", value_json);
     json_array_append(payload_data, command_json);
@@ -51,6 +49,7 @@ void mb_data_message::request(int reg, const char *command, const char *type, co
     json_decref(root);
     json_decref(payload);
     json_decref(payload_data);
+
     free(s);
 }
 
