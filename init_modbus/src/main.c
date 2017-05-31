74b3565725536f1d95b805f57ed779948dcc7cc6
Ian Tracy <iatr@hms.se>
2017-05-31 15:09:27 -0400

    new program init_modbus

---
+++ init_modbus/src/main.c
@@ -0,0 +1,71 @@
+#include <curl/curl.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+#include "nb/system.h"
+
+#include "request.h"
+
+char *parse_args(int argc, char *argv[]);
+
+int main(int argc, char *argv[])
+{
+    long response_code;
+    CURL *curl;
+    CURLcode curl_code;
+    uint64_t time_ms = 0;
+    char *name;
+
+    /* verify correct command line arguments */
+    name = parse_args(argc, argv);
+    if (name == NULL)
+        return 1;
+
+    /* perform initialization, and set options */
+    my_curl_init(name, &curl);
+
+    /* perform request; check for error */
+    curl_code = curl_easy_perform(curl);
+    if (curl_code != CURLE_OK) {
+        printf("Request error: %s\n", curl_easy_strerror(curl_code));
+        curl_easy_cleanup(curl);
+        curl_global_cleanup();
+        return 2;
+    }
+
+    /* check http response status code */
+    curl_code = curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &response_code);
+    if (response_code != 200) /* Http response code 200 = good response */
+    {
+        printf("Bad response: Code %ld \n", response_code);
+        curl_easy_cleanup(curl);
+        curl_global_cleanup();
+        return 3;
+    } else {
+        time_ms = get_time();
+        printf("The current time is  %lld \n", time_ms);
+        /* so we have a good response, what should we do  ? */
+        if( time != 0 ){
+          int res_nb = nb_system_set_time(time_ms);   
+        }
+    }
+
+    curl_easy_cleanup(curl);
+    curl_global_cleanup();
+
+    return 0;
+}
+
+char *parse_args(int argc, char *argv[])
+{
+    if (argc != 2) {
+        printf("usage %s url \n", argv[0]);
+    } else {
+        /*basic check for url format */
+        if (strncmp(argv[1], "https://", 8) == 0)
+            return argv[1];
+        else
+            printf("argument should be a url in the form https://...\n");
+    }
+    return NULL;
+}
---
+++ init_modbus/src/request.c
@@ -0,0 +1,37 @@
+#include <curl/curl.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+#include "request.h"
+
+static uint64_t time_ms = 0;
+
+size_t rspCallback(void *buffer, size_t size, size_t nmemb, void *userp)
+{
+    char *pEnd;
+    if (size * nmemb == 13) {
+        time_ms = strtoll((const char *)buffer, &pEnd, 10);
+        /* Sanity check for time  - sometime between 2016 and 2030 */
+        if (!((1450656000000 < time_ms) && (time_ms < 1892160000000)))
+            time_ms = 0;
+    }
+    return nmemb * size;
+}
+
+uint64_t get_time()
+{
+    return time_ms;
+}
+
+int my_curl_init(char *target_server, CURL **curl)
+{
+    *curl = curl_easy_init();
+    if (*curl) {
+        curl_easy_setopt(*curl, CURLOPT_URL, target_server);
+        curl_easy_setopt(*curl, CURLOPT_SSL_VERIFYPEER, 0  /*1L*/);
+        curl_easy_setopt(*curl, CURLOPT_SSL_VERIFYHOST, 0/*2L*/);
+        curl_easy_setopt(*curl, CURLOPT_WRITEFUNCTION, rspCallback);
+        return 0;
+    }
+    return 1;
+}
