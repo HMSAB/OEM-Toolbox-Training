e02d813c92d761ce2236e8ffe824d12f906a5493
Ian Tracy <iatr@hms.se>
2017-07-10 13:52:22 -0400

    1st commit

---
+++ run.sh
@@ -0,0 +1,10 @@
+# * Open a console and set the following environment variable:
+#   * `set Azure.IoT.IoTHub.ConnectionString=<your connection string>`
+#   * `set Azure.IoT.IoTHub.ConsumerGroup=<your consumer group name>`
+# * `npm install`
+# * `npm start`
+
+
+export Azure.IoT.IoTHub.ConnectionString="HostName=DirTeamTraining.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=d4kGVcfvaG/Ry3notfXK1zbeZct3iCwU3HR67yRwyWI="
+export Azure.IoT.IoTHub.ConsumerGroup="testconsumer"
+npm start 
